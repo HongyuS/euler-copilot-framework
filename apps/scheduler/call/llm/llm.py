6c27206ea021ba88a772b6f5d452e0da8cbf89d1
z30057876 <zhouweitong@huawei.com>
2025-04-07 15:39:03 +0800

    更新LLM工具

--- apps/scheduler/call/llm/llm.py
+++ apps/scheduler/call/llm/llm.py
@@ -1,54 +1,54 @@
-"""工具：调用大模型
-
-Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
 """
+工具：调用大模型
+
+Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
+"""
+
 import logging
 from collections.abc import AsyncGenerator
 from datetime import datetime
-from typing import Any, ClassVar
+from typing import Annotated, Any, ClassVar
 
 import pytz
-from jinja2 import BaseLoader, select_autoescape
+from jinja2 import BaseLoader
 from jinja2.sandbox import SandboxedEnvironment
-from pydantic import BaseModel, Field
+from pydantic import Field
 
-from apps.constants import LLM_CONTEXT_PROMPT, LLM_DEFAULT_PROMPT
-from apps.entities.scheduler import CallError, CallVars
+from apps.entities.enum_var import CallOutputType
+from apps.entities.scheduler import CallError, CallOutputChunk, CallVars
 from apps.llm.reasoning import ReasoningLLM
 from apps.scheduler.call.core import CoreCall
+from apps.scheduler.call.llm.schema import LLM_CONTEXT_PROMPT, LLM_DEFAULT_PROMPT, LLMInput, LLMOutput
 
-logger = logging.getLogger("ray")
-class LLMNodeOutput(BaseModel):
-    """定义LLM工具调用的输出"""
-
-    message: str = Field(description="大模型输出的文字信息")
+logger = logging.getLogger(__name__)
 
 
-class LLM(CoreCall, ret_type=LLMNodeOutput):
+class LLM(CoreCall, input_type=LLMInput, output_type=LLMOutput):
     """大模型调用工具"""
 
-    name: ClassVar[str] = "大模型"
-    description: ClassVar[str] = "以指定的提示词和上下文信息调用大模型，并获得输出。"
+    name: ClassVar[Annotated[str, Field(description="工具名称", exclude=True, frozen=True)]] = "大模型"
+    description: ClassVar[Annotated[str, Field(description="工具描述", exclude=True, frozen=True)]] = (
+        "以指定的提示词和上下文信息调用大模型，并获得输出。"
+    )
 
     temperature: float = Field(description="大模型温度（随机化程度）", default=0.7)
     enable_context: bool = Field(description="是否启用上下文", default=True)
-    step_history_size: int = Field(description="上下文信息中包含的步骤历史数量", default=3)
+    step_history_size: int = Field(description="上下文信息中包含的步骤历史数量", default=3, ge=1, le=10)
     system_prompt: str = Field(description="大模型系统提示词", default="")
     user_prompt: str = Field(description="大模型用户提示词", default=LLM_DEFAULT_PROMPT)
-
 
     async def _prepare_message(self, syscall_vars: CallVars) -> list[dict[str, Any]]:
         """准备消息"""
         # 创建共享的 Environment 实例
         env = SandboxedEnvironment(
             loader=BaseLoader(),
-            autoescape=select_autoescape(),
+            autoescape=False,
             trim_blocks=True,
             lstrip_blocks=True,
         )
 
         # 上下文信息
-        step_history = list(syscall_vars.history.values())[-self.step_history_size:]
+        step_history = list(syscall_vars.history.values())[-self.step_history_size :]
         if self.enable_context:
             context_tmpl = env.from_string(LLM_CONTEXT_PROMPT)
             context_prompt = context_tmpl.render(
@@ -82,34 +82,23 @@ class LLM(CoreCall, ret_type=LLMNodeOutput):
             {"role": "user", "content": user_input},
         ]
 
-
-    async def init(self, syscall_vars: CallVars, **_kwargs: Any) -> dict[str, Any]:
+    async def _init(self, syscall_vars: CallVars) -> dict[str, Any]:
         """初始化LLM工具"""
-        self._message = await self._prepare_message(syscall_vars)
-        self._task_id = syscall_vars.task_id
-        return {
-            "task_id": self._task_id,
-            "message": self._message,
-        }
+        await super()._init(syscall_vars)
 
+        return LLMInput(
+            task_id=syscall_vars.task_id,
+            message=await self._prepare_message(syscall_vars),
+        ).model_dump(exclude_none=True, by_alias=True)
 
-    async def exec(self) -> dict[str, Any]:
+    async def _exec(self, input_data: dict[str, Any]) -> AsyncGenerator[CallOutputChunk, None]:
         """运行LLM Call"""
+        data = LLMInput(**input_data)
         try:
-            result = ""
-            async for chunk in ReasoningLLM().call(task_id=self._task_id, messages=self._message):
-                result += chunk
+            # TODO: 临时改成了非流式
+            async for chunk in ReasoningLLM().call(task_id=data.task_id, messages=data.message, streaming=False):
+                if not chunk:
+                    continue
+                yield CallOutputChunk(type=CallOutputType.TEXT, content=chunk)
         except Exception as e:
             raise CallError(message=f"大模型调用失败：{e!s}", data={}) from e
-
-        result = result.strip().strip("\n")
-        return LLMNodeOutput(message=result).model_dump(exclude_none=True, by_alias=True)
-
-
-    async def stream(self) -> AsyncGenerator[str, None]:
-        """流式输出"""
-        try:
-            async for chunk in ReasoningLLM().call(task_id=self._task_id, messages=self._message):
-                yield chunk
-        except Exception as e:
-            raise CallError(message=f"大模型流式输出失败：{e!s}", data={}) from e
