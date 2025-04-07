bd1e49b7612e1da03d652babe70e189847f111db
z30057876 <zhouweitong@huawei.com>
2025-04-07 10:48:09 +0800

    新版Activity，不依赖Redis

--- apps/service/activity.py
+++ apps/service/activity.py
@@ -1,62 +1,75 @@
-"""用户限流
-
-Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
 """
-from datetime import datetime, timezone
+用户限流
+
+Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
+"""
+
+import uuid
+from datetime import UTC, datetime
 
 from apps.constants import SLIDE_WINDOW_QUESTION_COUNT, SLIDE_WINDOW_TIME
-from apps.models.redis import RedisConnectionPool
+from apps.exceptions import ActivityError
+from apps.models.mongo import MongoDB
 
-_SLIDE_WINDOW_KEY = "slide_window"
 
 class Activity:
     """用户活动控制，限制单用户同一时间只能提问一个问题"""
 
     @staticmethod
     async def is_active(user_sub: str) -> bool:
-        """判断当前用户是否正在提问（占用GPU资源）
+        """
+        判断当前用户是否正在提问（占用GPU资源）
 
         :param user_sub: 用户实体ID
         :return: 判断结果，正在提问则返回True
         """
-        time = round(datetime.now(timezone.utc).timestamp(), 3)
-        async with RedisConnectionPool.get_redis_connection().pipeline(transaction=True) as pipe:
-            # 检查窗口内总请求数
-            pipe.zcount(_SLIDE_WINDOW_KEY, time - SLIDE_WINDOW_TIME, time)
-            result = await pipe.execute()
-            if result[0] >= SLIDE_WINDOW_QUESTION_COUNT:
-                # 服务器处理请求过多
-                return True
+        time = round(datetime.now(UTC).timestamp(), 3)
 
-            # 检查用户是否正在提问
-            pipe.get(f"{user_sub}_active")
-            result = await pipe.execute()
-            if result[0]:
-                return True
-        return False
+        # 检查窗口内总请求数
+        count = await MongoDB.get_collection("activity").count_documents(
+            {"timestamp": {"$gte": time - SLIDE_WINDOW_TIME, "$lte": time}},
+        )
+        if count >= SLIDE_WINDOW_QUESTION_COUNT:
+            return True
+
+        # 检查用户是否正在提问
+        active = await MongoDB.get_collection("activity").find_one(
+            {"user_sub": user_sub},
+        )
+        return bool(active)
 
     @staticmethod
     async def set_active(user_sub: str) -> None:
         """设置用户的活跃标识"""
-        time = round(datetime.now(timezone.utc).timestamp(), 3)
-        async with RedisConnectionPool.get_redis_connection().pipeline(transaction=True) as pipe:
-            # 设置限流
-            pipe.set(f"{user_sub}_active", 1)
-            pipe.expire(f"{user_sub}_active", 300)
-            pipe.zadd(_SLIDE_WINDOW_KEY, {f"{user_sub}_{time}": time})
-            await pipe.execute()
+        time = round(datetime.now(UTC).timestamp(), 3)
+        # 设置用户活跃状态
+        collection = MongoDB.get_collection("activity")
+        active = await collection.find_one({"user_sub": user_sub})
+        if active:
+            err = "用户正在提问"
+            raise ActivityError(err)
+        await collection.insert_one(
+            {
+                "_id": str(uuid.uuid4()),
+                "user_sub": user_sub,
+                "timestamp": time,
+            },
+        )
 
     @staticmethod
     async def remove_active(user_sub: str) -> None:
-        """清除用户的活跃标识，释放GPU资源
+        """
+        清除用户的活跃标识，释放GPU资源
 
         :param user_sub: 用户实体ID
         """
-        time = round(datetime.now(timezone.utc).timestamp(), 3)
-        async with RedisConnectionPool.get_redis_connection().pipeline(transaction=True) as pipe:
-            # 清除用户当前活动标识
-            pipe.delete(f"{user_sub}_active")
+        time = round(datetime.now(UTC).timestamp(), 3)
+        # 清除用户当前活动标识
+        await MongoDB.get_collection("activity").delete_one(
+            {"user_sub": user_sub},
+        )
 
-            # 清除超出窗口范围的请求记录
-            pipe.zremrangebyscore(_SLIDE_WINDOW_KEY, 0, time - SLIDE_WINDOW_TIME)
-            await pipe.execute()
+        # 清除超出窗口范围的请求记录
+        await MongoDB.get_collection("activity").delete_many(
+            {"timestamp": {"$lte": time - SLIDE_WINDOW_TIME}},
+        )
