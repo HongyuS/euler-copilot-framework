3faaadedc57b804aff308eae6a214241641edc0b
z30057876 <zhouweitong@huawei.com>
2025-04-07 11:46:11 +0800

    使用新版配置文件 & 固定data文件夹内子路径

--- apps/scheduler/pool/__init__.py
+++ apps/scheduler/pool/__init__.py
@@ -1,5 +1,6 @@
-"""数据池
+"""
+数据池
 
 包含Flow、Plugin、Call等的Loader
-Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
+Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
 """
