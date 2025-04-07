00931b6bd650068ed482e81b1446abd1d51c1746
z30057876 <zhouweitong@huawei.com>
2025-04-07 15:28:34 +0800

    更新CoreCall结构

--- apps/scheduler/call/empty.py
+++ apps/scheduler/call/empty.py
@@ -1,31 +1,40 @@
-"""空白Call
-
-Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
 """
+空白Call
+
+Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
+"""
+
+from collections.abc import AsyncGenerator
 from typing import Any, ClassVar
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
-from apps.entities.scheduler import CallVars
-from apps.scheduler.call.core import CoreCall
-
-
-class EmptyData(BaseModel):
-    """空数据"""
+from apps.entities.enum_var import CallOutputType
+from apps.entities.scheduler import CallOutputChunk, CallVars
+from apps.scheduler.call.core import CoreCall, DataBase
 
 
-class Empty(CoreCall, ret_type=EmptyData):
+class EmptyInput(BaseModel):
+    """空输入"""
+
+
+class EmptyOutput(DataBase):
+    """空输出"""
+
+
+class Empty(CoreCall, input_type=EmptyInput, output_type=EmptyOutput):
     """空Call"""
 
-    name: ClassVar[str] = "空白"
-    description: ClassVar[str] = "空白节点，用于占位"
+    name: ClassVar[str] = Field("空白", exclude=True, frozen=True)
+    description: ClassVar[str] = Field("空白节点，用于占位", exclude=True, frozen=True)
 
 
-    async def init(self, _syscall_vars: CallVars, **_kwargs: Any) -> dict[str, Any]:
+    async def _init(self, syscall_vars: CallVars) -> dict[str, Any]:
         """初始化"""
         return {}
 
 
-    async def exec(self) -> dict[str, Any]:
+    async def _exec(self, input_data: dict[str, Any]) -> AsyncGenerator[CallOutputChunk, None]:
         """执行"""
-        return EmptyData().model_dump()
+        output = CallOutputChunk(type=CallOutputType.TEXT, content="")
+        yield output
