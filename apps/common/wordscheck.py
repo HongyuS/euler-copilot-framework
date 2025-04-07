554d5af70472b89b5c17d039cedf9adf69c3e109
z30057876 <zhouweitong@huawei.com>
2025-04-07 10:24:59 +0800

    去掉使用API检查敏感词的流程，只保留敏感词列表

--- apps/common/wordscheck.py
+++ apps/common/wordscheck.py
@@ -1,65 +1,52 @@
-"""敏感词检查模块
-
-Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
 """
-import http
+敏感词检查模块
+
+Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
+"""
+
 import logging
-import re
+from pathlib import Path
 
-import aiofiles
-import aiohttp
-import ray
+from apps.common.config import Config
+from apps.common.singleton import SingletonMeta
 
-from apps.common.config import config
+logger = logging.getLogger(__name__)
 
-logger = logging.getLogger("ray")
 
-@ray.remote
-class WordsCheck:
+class WordsCheck(metaclass=SingletonMeta):
     """敏感词检查工具"""
 
-    async def _init_file(self) -> None:
-        """初始化关键词列表"""
-        async with aiofiles.open(config["WORDS_LIST"], encoding="utf-8") as f:
-            self.words_list = (await f.read()).splitlines()
+    def __init__(self) -> None:
+        """初始化"""
+        self._words_list: list[str] = []
+        self._initialized = False
+
+    def _init_words_list(self) -> None:
+        """同步初始化敏感词列表"""
+        if not self._initialized and Config().get_config().check.enable:
+            try:
+                with Path(Config().get_config().check.words_list).open(encoding="utf-8") as f:
+                    self._words_list = f.read().splitlines()
+                self._initialized = True
+            except Exception:
+                logger.exception("[WordsCheck] Failed to initialize words list")
 
     async def _check_wordlist(self, message: str) -> int:
         """使用关键词列表检查敏感词"""
-        if message in self.words_list:
-            return 1
+        if not self._initialized:
+            self._init_words_list()
+        for word in self._words_list:
+            if word in message:
+                return 1
         return 0
 
-    async def _check_api(self, message: str) -> int:
-        """使用API接口检查敏感词"""
-        url = config["WORDS_CHECK"]
-        if url is None:
-            err = "配置文件中未设置WORDS_CHECK"
-            raise ValueError(err)
-
-        headers = {"Content-Type": "application/json"}
-        data = {"content": message}
-        try:
-            async with aiohttp.ClientSession() as session, session.post(url=url, json=data, headers=headers, timeout=10) as response:
-                if response.status == http.HTTPStatus.OK and re.search("ok", str(response.content)):
-                    return 1
-                return 0
-        except Exception:
-            logger.exception("[WordsCheck] 过滤敏感词错误")
-            return -1
-
-    async def init(self) -> None:
-        """初始化敏感词检查器"""
-        if config["DETECT_TYPE"] == "keyword":
-            await self._init_file()
-
     async def check(self, message: str) -> int:
-        """检查消息是否包含关键词
+        """
+        检查消息是否包含关键词
 
         异常-1，拦截0，正常1
         """
-        if config["DETECT_TYPE"] == "keyword":
+        if Config().get_config().check.enable:
             return await self._check_wordlist(message)
-        if config["DETECT_TYPE"] == "wordscheck":
-            return await self._check_api(message)
         # 不设置检查类型，默认不拦截
         return 1
