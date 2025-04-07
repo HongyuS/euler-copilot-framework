8f9054657d54e1a9ac082977b36b00ed770321ed
z30057876 <zhouweitong@huawei.com>
2025-04-07 11:48:52 +0800

    去除AppLoader的向量化步骤

--- apps/scheduler/pool/loader/__init__.py
+++ apps/scheduler/pool/loader/__init__.py
@@ -1,7 +1,9 @@
-"""配置加载器
-
-Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
 """
+配置加载器
+
+Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
+"""
+
 from apps.scheduler.pool.loader.app import AppLoader
 from apps.scheduler.pool.loader.call import CallLoader
 from apps.scheduler.pool.loader.flow import FlowLoader
