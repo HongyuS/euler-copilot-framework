2d7cc9dc7af60179adc8494be0279258b980c04b
z30057876 <zhouweitong@huawei.com>
2025-04-07 10:56:11 +0800

    CleanCode & 使用新版配置文件

--- apps/models/mongo.py
+++ apps/models/mongo.py
@@ -1,7 +1,9 @@
-"""MongoDB 连接
-
-Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
 """
+MongoDB 连接
+
+Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
+"""
+
 from __future__ import annotations
 
 import logging
@@ -10,9 +12,9 @@ from typing import TYPE_CHECKING
 
 from pymongo import AsyncMongoClient
 
-from apps.common.config import config
+from apps.common.config import Config
 
-logger = logging.getLogger("ray")
+logger = logging.getLogger(__name__)
 
 
 if TYPE_CHECKING:
@@ -24,14 +26,14 @@ class MongoDB:
     """MongoDB连接"""
 
     _client: AsyncMongoClient = AsyncMongoClient(
-        f"mongodb://{urllib.parse.quote_plus(config['MONGODB_USER'])}:{urllib.parse.quote_plus(config['MONGODB_PWD'])}@{config['MONGODB_HOST']}:{config['MONGODB_PORT']}/?directConnection=true&replicaSet=rs0",
+        f"mongodb://{urllib.parse.quote_plus(Config().get_config().mongodb.user)}:{urllib.parse.quote_plus(Config().get_config().mongodb.password)}@{Config().get_config().mongodb.host}:{Config().get_config().mongodb.port}/?directConnection=true&replicaSet=rs0",
     )
 
     @classmethod
     def get_collection(cls, collection_name: str) -> AsyncCollection:
         """获取MongoDB集合（表）"""
         try:
-            return cls._client[config["MONGODB_DATABASE"]][collection_name]
+            return cls._client[Config().get_config().mongodb.database][collection_name]
         except Exception as e:
             logger.exception("[MongoDB] 获取集合 %s 失败", collection_name)
             raise RuntimeError(str(e)) from e
@@ -40,7 +42,7 @@ class MongoDB:
     async def clear_collection(cls, collection_name: str) -> None:
         """清空MongoDB集合（表）"""
         try:
-            await cls._client[config["MONGODB_DATABASE"]][collection_name].delete_many({})
+            await cls._client[Config().get_config().mongodb.database][collection_name].delete_many({})
         except Exception:
             logger.exception("[MongoDB] 清空集合 %s 失败", collection_name)
 
