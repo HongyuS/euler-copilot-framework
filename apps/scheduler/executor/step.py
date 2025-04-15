12aa2d82ee5744ddbaf1746268bef4f0982f4cd1
z30057876 <zhouweitong@huawei.com>
2025-04-15 11:46:12 +0800

    StepExecutor使用queue，彻底剥离填参逻辑

--- apps/scheduler/executor/step.py
+++ apps/scheduler/executor/step.py
@@ -1,23 +1,19 @@
 """工作流中步骤相关函数"""
 
 import logging
-from typing import TYPE_CHECKING, Any
+from typing import Any
 
 from pydantic import ConfigDict, Field
 
 from apps.common.queue import MessageQueue
 from apps.entities.enum_var import EventType, StepStatus
 from apps.entities.flow import Step
+from apps.entities.scheduler import CallOutputChunk
 from apps.entities.task import FlowStepHistory, Task
 from apps.manager.node import NodeManager
 from apps.manager.task import TaskManager
-from apps.scheduler.call.slot.schema import SlotInput, SlotOutput
-from apps.scheduler.call.slot.slot import Slot
 from apps.scheduler.executor.base import BaseExecutor
 from apps.scheduler.executor.node import StepNode
-
-if TYPE_CHECKING:
-    from apps.entities.scheduler import CallOutputChunk
 
 logger = logging.getLogger(__name__)
 SPECIAL_EVENT_TYPES = [
@@ -84,7 +80,7 @@ class StepExecutor(BaseExecutor):
             logger.error(err)
             raise ValueError(err)
 
-        logger.info("[FlowExecutor] 运行步骤 %s", self.step.name)
+        logger.info("[StepExecutor] 初始化步骤 %s", self.step.name)
 
         # State写入ID和运行状态
         self.task.state.step_id = self.step_id
@@ -97,8 +93,8 @@ class StepExecutor(BaseExecutor):
         # 获取node详情并存储
         try:
             self._node_data = await NodeManager.get_node(node_id)
-        except Exception:
-            logger.exception("[StepExecutor] 获取Node失败，为内部Node或ID不存在", stack_info=False)
+        except ValueError:
+            logger.info("[StepExecutor] 获取Node失败，为内部Node或ID不存在")
             self._node_data = None
 
         if self._node_data:
@@ -116,76 +112,43 @@ class StepExecutor(BaseExecutor):
         if self.step.params:
             params.update(self.step.params)
 
-        # 检查初始化Call Class是否需要特殊处理
         try:
-            self._obj, self._input = await call_cls.init(self, **params)
+            self.obj, self.input = await call_cls.init(self, **params)
         except Exception:
             logger.exception("[StepExecutor] 初始化Call失败")
             raise
 
 
-    async def fill_slots(self) -> dict[str, Any]:
-        """执行Call并处理结果"""
+    async def run_step(self, *, to_user: bool = False) -> None:
+        """运行单个步骤"""
         if not self.task.state:
             err = "[StepExecutor] 当前ExecutorState为空"
             logger.error(err)
             raise ValueError(err)
+
+        logger.info("[StepExecutor] 运行步骤 %s", self.step.name)
 
         # 合并已经填充的参数
-        self._input.update(self.task.runtime.filled)
+        self.input.update(self.task.runtime.filled)
+        await self.push_message(EventType.STEP_INPUT, self.input)
 
-        # 检查输入参数是否需要填充
-        input_schema = (
-            self._obj.input_type.model_json_schema(override=self._node_data.override_input) if self._node_data else {}
-        )
-        slot_obj = Slot(
-            data=self._input,
-            current_schema=input_schema,
-            summary=self.task.runtime.summary,
-            facts=self.task.runtime.facts,
-        )
-        slot_input = SlotInput(**await slot_obj.init(self))
+        # 执行步骤
+        iterator = self.obj.exec(self, self.input)
 
-        # 若需要填参，额外执行一步
-        if slot_input.remaining_schema:
-            output = SlotOutput(**await self.run_step("Slot", slot_obj, slot_input.remaining_schema))
-            self.task.runtime.filled = output.slot_data
+        self.answer = ""
+        self.content = {}
+        if not to_user:
+            async for chunk in iterator:
+                if not isinstance(chunk, CallOutputChunk):
+                    err = "[StepExecutor] 返回结果类型错误"
+                    logger.error(err)
+                    raise TypeError(err)
 
-            # 如果还有剩余参数，则中断
-            if output.remaining_schema:
-                # 更新状态
-                self.task.state.status = StepStatus.PARAM
-                self.task.state.slot = output.remaining_schema
-                err = "[StepExecutor] 需要填参"
-                logger.error(err)
-                raise ValueError(err)
-
-            return output.slot_data
-        return self._input
-
-
-    # TODO: 需要评估如何进行流式输出
-    async def run_step(self, input_data: dict[str, Any]) -> dict[str, Any]:
-        """运行单个步骤"""
-        iterator = self._obj.exec(self, input_data)
-        await self.push_message(EventType.STEP_INPUT, input_data)
-
-        result: CallOutputChunk | None = None
-        async for chunk in iterator:
-            result = chunk
-
-        if result is None:
-            content = {}
-        else:
-            content = result.content if isinstance(result.content, dict) else {"message": result.content}
+                if isinstance(chunk.content, str):
+                    self.answer += chunk.content
+                else:
+                    self.content = chunk.content
 
         # 更新执行状态
-        if not self.task.state:
-            err = "[StepExecutor] 当前ExecutorState为空"
-            logger.error(err)
-            raise ValueError(err)
-
         self.task.state.status = StepStatus.SUCCESS
-        await self.push_message(EventType.STEP_OUTPUT, content)
-
-        return content
+        await self.push_message(EventType.STEP_OUTPUT, self.content)
