4372bd4e39938af03fa6d90b8ad73e2c9f87926c
z30057876 <zhouweitong@huawei.com>
2025-04-30 11:27:16 +0800

    调整search工具结构

--- apps/scheduler/call/search/search.py
+++ apps/scheduler/call/search/search.py
@@ -1,25 +1,33 @@
 """搜索工具"""
 
-from typing import Any, ClassVar
+from collections.abc import AsyncGenerator
+from typing import Any
 
-from apps.entities.scheduler import CallVars
+from apps.entities.scheduler import (
+    CallError,
+    CallInfo,
+    CallOutputChunk,
+    CallVars,
+)
 from apps.scheduler.call.core import CoreCall
-from apps.scheduler.call.search.schema import SearchInput, SearchRet
+from apps.scheduler.call.search.schema import SearchInput, SearchOutput
 
 
-class Search(CoreCall, input_model=SearchInput, output_model=SearchRet):
+class Search(CoreCall, input_model=SearchInput, output_model=SearchOutput):
     """搜索工具"""
 
-    name: ClassVar[str] = "搜索"
-    description: ClassVar[str] = "获取搜索引擎的结果"
+    @classmethod
+    def info(cls) -> CallInfo:
+        """返回Call的名称和描述"""
+        return CallInfo(name="搜索", description="获取搜索引擎的结果")
 
-    async def _init(self, call_vars: CallVars, **kwargs: Any) -> SearchInput:
+
+    async def _init(self, call_vars: CallVars) -> SearchInput:
         """初始化工具"""
-        self._query: str = kwargs["query"]
-        return SearchInput(query=self._query)
+        pass
 
 
-    async def _exec(self) -> dict[str, Any]:
+    async def _exec(self, input_data: dict[str, Any]) -> AsyncGenerator[CallOutputChunk, None]:
         """执行工具"""
-        return {}
+        pass
 
