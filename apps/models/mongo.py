7142a279e677f3816dabdce16eb778347a31096e
z30057876 <zhouweitong@huawei.com>
2025-05-08 15:37:08 +0800

    增加数据库模块文档

--- apps/models/mongo.py
+++ apps/models/mongo.py
@@ -1,8 +1,5 @@
-"""
-MongoDB 连接
-
-Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
-"""
+# Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
+"""MongoDB 连接器"""
 
 from __future__ import annotations
 
@@ -23,15 +20,22 @@ if TYPE_CHECKING:
 
 
 class MongoDB:
-    """MongoDB连接"""
+    """MongoDB连接器"""
 
     _client: AsyncMongoClient = AsyncMongoClient(
         f"mongodb://{urllib.parse.quote_plus(Config().get_config().mongodb.user)}:{urllib.parse.quote_plus(Config().get_config().mongodb.password)}@{Config().get_config().mongodb.host}:{Config().get_config().mongodb.port}/?directConnection=true&replicaSet=rs0",
     )
+    """异步的MongoDB Client"""
 
     @classmethod
     def get_collection(cls, collection_name: str) -> AsyncCollection:
-        """获取MongoDB集合（表）"""
+        """
+        获取MongoDB集合
+
+        :param str collection_name: 集合名称
+        :return: 集合对象
+        :rtype: AsyncCollection
+        """
         try:
             return cls._client[Config().get_config().mongodb.database][collection_name]
         except Exception as e:
@@ -40,7 +44,12 @@ class MongoDB:
 
     @classmethod
     async def clear_collection(cls, collection_name: str) -> None:
-        """清空MongoDB集合（表）"""
+        """
+        清空MongoDB集合
+
+        :param str collection_name: 集合名称
+        :return: 无
+        """
         try:
             await cls._client[Config().get_config().mongodb.database][collection_name].delete_many({})
         except Exception:
@@ -48,5 +57,12 @@ class MongoDB:
 
     @classmethod
     def get_session(cls) -> AsyncClientSession:
-        """获取MongoDB会话"""
+        """
+        获取MongoDB会话
+
+        一个Client可以创建多个会话，一个会话一般用于一个事务。
+
+        :return: 会话对象
+        :rtype: AsyncClientSession
+        """
         return cls._client.start_session()
