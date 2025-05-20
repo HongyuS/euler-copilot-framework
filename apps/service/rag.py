6ff4f26d70b816a7a8a95474012c8374d5e1d4e7
z30057876 <zhouweitong@huawei.com>
2025-05-20 17:29:23 +0800

    修正RAG问题

--- apps/service/rag.py
+++ apps/service/rag.py
@@ -15,6 +15,7 @@ from apps.entities.config import LLMConfig
 from apps.entities.rag_data import RAGQueryReq
 from apps.llm.patterns.rewrite import QuestionRewrite
 from apps.llm.reasoning import ReasoningLLM
+from apps.llm.token import TokenCalculator
 from apps.manager.session import SessionManager
 from apps.service import Activity
 
@@ -43,42 +44,42 @@ class RAG:
     """
 
     @staticmethod
-    def get_tokens(content: str) -> int:
-        try:
-            enc = tiktoken.encoding_for_model("gpt-4")
-            return len(enc.encode(str(content)))
-        except Exception as e:
-            err = f"[TokenTool] 获取token失败 {e}"
-            logging.exception("[TokenTool] %s", err)
-        return 0
+    def get_k_tokens_words_from_content(content: str, k: int | None = None) -> str:
+        """获取k个token的词"""
+        if k is None:
+            return content
 
-    @staticmethod
-    def get_k_tokens_words_from_content(content: str, k: int = 16) -> list:
         try:
-            if RAG.get_tokens(content) <= k:
+            if TokenCalculator().calculate_token_length(messages=[
+                {"role": "user", "content": content},
+            ]) <= k:
                 return content
             l = 0
             r = len(content)
             while l + 1 < r:
                 mid = (l + r) // 2
-                if RAG.get_tokens(content[:mid]) <= k:
+                if TokenCalculator().calculate_token_length(messages=[
+                    {"role": "user", "content": content[:mid]},
+                ]) <= k:
                     l = mid
                 else:
                     r = mid
             return content[:l]
-        except Exception as e:
-            err = f"[RAG] 获取k个token的词失败 {e}"
-            logging.exception("[RAG] %s", err)
+        except Exception:
+            logger.exception("[RAG] 获取k个token的词失败")
         return ""
 
     @staticmethod
     async def get_rag_result(
-        user_sub: str, llm: LLM, history: list[dict[str, str]], data: RAGQueryReq
+        user_sub: str, llm: LLM, history: list[dict[str, str]], data: RAGQueryReq,
     ) -> AsyncGenerator[str, None]:
         """获取RAG服务的结果"""
         session_id = await SessionManager.get_session_by_user_sub(user_sub)
         url = Config().get_config().rag.rag_service.rstrip("/") + "/chunk/search"
-        headers = {"Content-Type": "application/json", "Authorization": f"Bearer {session_id}"}
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {session_id}",
+        }
         data.tokens_limit = llm.max_tokens
         llm_config = LLMConfig(
             endpoint=llm.openai_base_url,
@@ -90,8 +91,9 @@ class RAG:
             try:
                 question_obj = QuestionRewrite()
                 data.query = await question_obj.generate(history=history, question=data.query)
-            except Exception as e:
-                logger.error("[RAG] 问题重写失败: %s", e)
+            except Exception:
+                logger.exception("[RAG] 问题重写失败")
+
         reasion_llm = ReasoningLLM(llm_config)
         corpus = []
         async with httpx.AsyncClient() as client:
@@ -104,13 +106,18 @@ class RAG:
                 for doc_chunk in doc_chunk_list:
                     for chunk in doc_chunk["chunks"]:
                         corpus.append(chunk["text"].replace("\n", ""))
+
         text = ""
         for i in range(len(corpus)):
             text += corpus[i] + "\n"
         text = RAG.get_k_tokens_words_from_content(text, llm.max_tokens)
 
-        messages = history + [
-            {"role": "system", "content": RAG.system_prompt},
+        messages = [
+            *history,
+            {
+                "role": "system",
+                "content": RAG.system_prompt,
+            },
             {
                 "role": "user",
                 "content": RAG.user_prompt.format(
@@ -119,14 +126,22 @@ class RAG:
                 ),
             },
         ]
-        input_tokens = RAG.get_tokens(text)
+        input_tokens = TokenCalculator().calculate_token_length(messages=messages)
         output_tokens = 0
         async for chunk in reasion_llm.call(
-            messages, max_tokens=llm.max_tokens, streaming=True, temperature=0.7, result_only=False
+            messages,
+            max_tokens=llm.max_tokens,
+            streaming=True,
+            temperature=0.7,
+            result_only=False,
         ):
             if not await Activity.is_active(user_sub):
                 return
-            output_tokens += RAG.get_tokens(chunk)
+            output_tokens += TokenCalculator().calculate_token_length(
+                messages=[
+                    {"role": "assistant", "content": chunk},
+                ],
+            )
             yield (
                 "data: "
                 + json.dumps(
