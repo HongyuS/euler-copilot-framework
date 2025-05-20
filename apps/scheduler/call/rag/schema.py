fd726f710505b0103327dbf681f9fb25d1bd9967
zxstty <zhaojiaqi18@huawei.com>
2025-05-20 16:23:07 +0800

    修复rag call的bug

--- apps/scheduler/call/rag/schema.py
+++ apps/scheduler/call/rag/schema.py
@@ -2,6 +2,8 @@
 
 import uuid
 
+from typing import Optional
+
 from enum import Enum
 
 from pydantic import Field, BaseModel
@@ -32,10 +34,10 @@ class RAGInput(DataBase):
     knowledge_base_ids: list[uuid.UUID] = Field(description="知识库的id列表", default=[], alias="kbIds")
     top_k: int = Field(description="返回的分片数量", default=5, alias="topK")
     question: str = Field(description="用户输入", default="", alias="query")
-    document_ids: list[uuid.UUID] = Field(description="文档id列表", default=[], alias="docIds")
+    document_ids: Optional[list[uuid.UUID]] = Field(description="文档id列表", default=None, alias="docIds")
     search_method: str = Field(description="检索方法", default=SearchMethod.KEYWORD_AND_VECTOR.value, alias="searchMethod")
     is_related_surrounding: bool = Field(description="是否关联上下文", default=True, alias="isRelatedSurrounding")
-    is_classify_by_doc: bool = Field(description="是否按文档分类", default=False, alias="isClassifyByDoc")
+    is_classify_by_doc: bool = Field(description="是否按文档分类", default=True, alias="isClassifyByDoc")
     is_rerank: bool = Field(description="是否重新排序", default=False, alias="isRerank")
     is_compress: bool = Field(description="是否压缩", default=False, alias="isCompress")
     tokens_limit: int = Field(description="token限制", default=8192, alias="tokensLimit")
