756a457640a413ac93ece9524bd50e70f556a3a7
zxstty <zhaojiaqi18@huawei.com>
2025-05-20 16:11:28 +0800

    修复创建时app type未赋值的问题

--- apps/manager/appcenter.py
+++ apps/manager/appcenter.py
@@ -212,8 +212,9 @@ class AppCenterManager:
         app_id = str(uuid.uuid4())
         if data.app_type == AppType.FLOW:
             metadata = AppMetadata(
+                app_type=data.app_type,
                 type=MetadataType.APP,
-                   id=app_id,
+                id=app_id,
                 icon=data.icon,
                 name=data.name,
                 description=data.description,
@@ -229,6 +230,7 @@ class AppCenterManager:
             )
         elif data.app_type == AppType.AGENT:
             metadata = AgentAppMetadata(
+                app_type=data.app_type,
                 type=MetadataType.APP,
                 id=app_id,
                 icon=data.icon,
