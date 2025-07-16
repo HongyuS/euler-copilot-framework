8ad636cf6e401b355841f33b839d8ca1a52a2d3d
z30057876 <zhouweitong@huawei.com>
2025-07-16 15:18:37 +0800

    删除MCP中的冗余代码

--- apps/scheduler/mcp/select.py
+++ apps/scheduler/mcp/select.py
@@ -3,21 +3,15 @@
 
 import logging
 
-from jinja2 import BaseLoader
-from jinja2.sandbox import SandboxedEnvironment
-
 from apps.common.lance import LanceDB
 from apps.common.mongo import MongoDB
 from apps.llm.embedding import Embedding
 from apps.llm.function import FunctionLLM
 from apps.llm.reasoning import ReasoningLLM
 from apps.schemas.mcp import (
-    MCPCollection,
     MCPSelectResult,
     MCPTool,
 )
-
-from .prompt import MCP_SELECT
 
 logger = logging.getLogger(__name__)
 
@@ -37,67 +31,6 @@ class MCPSelector:
         for mcp_id in mcp_list:
             sql += f"'{mcp_id}', "
         return sql.rstrip(", ") + ")"
-
-
-    async def _get_top_mcp_by_embedding(
-        self,
-        query: str,
-        mcp_list: list[str],
-    ) -> list[dict[str, str]]:
-        """通过向量检索获取Top5 MCP Server"""
-        logger.info("[MCPHelper] 查询MCP Server向量: %s, %s", query, mcp_list)
-        mcp_table = await LanceDB().get_table("mcp")
-        query_embedding = await Embedding.get_embedding([query])
-        mcp_vecs = await (await mcp_table.search(
-            query=query_embedding,
-            vector_column_name="embedding",
-        )).where(f"id IN {MCPSelector._assemble_sql(mcp_list)}").limit(5).to_list()
-
-        # 拿到名称和description
-        logger.info("[MCPHelper] 查询MCP Server名称和描述: %s", mcp_vecs)
-        mcp_collection = MongoDB().get_collection("mcp")
-        llm_mcp_list: list[dict[str, str]] = []
-        for mcp_vec in mcp_vecs:
-            mcp_id = mcp_vec["id"]
-            mcp_data = await mcp_collection.find_one({"_id": mcp_id})
-            if not mcp_data:
-                logger.warning("[MCPHelper] 查询MCP Server名称和描述失败: %s", mcp_id)
-                continue
-            mcp_data = MCPCollection.model_validate(mcp_data)
-            llm_mcp_list.extend([{
-                "id": mcp_id,
-                "name": mcp_data.name,
-                "description": mcp_data.description,
-            }])
-        return llm_mcp_list
-
-
-    async def _get_mcp_by_llm(
-        self,
-        query: str,
-        mcp_list: list[dict[str, str]],
-        mcp_ids: list[str],
-    ) -> MCPSelectResult:
-        """通过LLM选择最合适的MCP Server"""
-        # 初始化jinja2环境
-        env = SandboxedEnvironment(
-            loader=BaseLoader,
-            autoescape=True,
-            trim_blocks=True,
-            lstrip_blocks=True,
-        )
-        template = env.from_string(MCP_SELECT)
-        # 渲染模板
-        mcp_prompt = template.render(
-            mcp_list=mcp_list,
-            goal=query,
-        )
-
-        # 调用大模型进行推理
-        result = await self._call_reasoning(mcp_prompt)
-
-        # 使用小模型提取JSON
-        return await self._call_function_mcp(result, mcp_ids)
 
 
     async def _call_reasoning(self, prompt: str) -> str:
@@ -134,23 +67,6 @@ class MCPSelector:
             logger.exception("[MCPHelper] 解析MCP Select Result失败")
             raise
         return result
-
-
-    async def select_top_mcp(
-        self,
-        query: str,
-        mcp_list: list[str],
-    ) -> MCPSelectResult:
-        """
-        选择最合适的MCP Server
-
-        先通过Embedding选择Top5，然后通过LLM选择Top 1
-        """
-        # 通过向量检索获取Top5
-        llm_mcp_list = await self._get_top_mcp_by_embedding(query, mcp_list)
-
-        # 通过LLM选择最合适的
-        return await self._get_mcp_by_llm(query, llm_mcp_list, mcp_list)
 
 
     @staticmethod
