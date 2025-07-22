49b1d78e8604af762358e77aab898d22dd80922e
zxstty <zhaojiaqi18@huawei.com>
2025-07-22 15:14:29 +0800

    拆分问答场景下rag的类，完善问题改写的类（history压缩在一轮中）

--- apps/llm/patterns/rewrite.py
+++ apps/llm/patterns/rewrite.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel, Field
 from apps.llm.function import JsonGenerator
 from apps.llm.patterns.core import CorePattern
 from apps.llm.reasoning import ReasoningLLM
-
+from apps.llm.token import TokenCalculator
 logger = logging.getLogger(__name__)
 
 
@@ -27,7 +27,7 @@ class QuestionRewrite(CorePattern):
     user_prompt: str = r"""
         <instructions>
           <instruction>
-            根据上面的对话，推断用户的实际意图并补全用户的提问内容。
+            根据历史对话，推断用户的实际意图并补全用户的提问内容,历史对话被包含在<history>标签中，用户意图被包含在<question>标签中。
             要求：
               1. 请使用JSON格式输出，参考下面给出的样例；不要包含任何XML标签，不要包含任何解释说明；
               2. 若用户当前提问内容与对话上文不相关，或你认为用户的提问内容已足够完整，请直接输出用户的提问内容。
@@ -40,17 +40,41 @@ class QuestionRewrite(CorePattern):
           </instruction>
 
           <example>
-            <input>openEuler的特点？</input>
+            <history>
+              <qa>
+                <question>
+                  openEuler的特点是什么？
+                </question>
+                <answer>
+                  openEuler相较于其他操作系统，其特点是支持多种硬件架构，并且提供稳定、安全、高效的操作系统平台。
+                </answer>
+              </qa>
+              <qa>
+                <question>
+                  openEuler的优势有哪些？
+                </question>
+                <answer>
+                  openEuler的优势包括开源、社区支持、以及对云计算和边缘计算的优化。
+                </answer>
+              </qa>
+            </history>
+
+            <question>
+              详细点？
+            </question>
             <output>
               {{
-                "question": "openEuler相较于其他操作系统，其特点是什么？"
+                "question": "openEuler的特点是什么？请详细说明其优势和应用场景。"
               }}
             </output>
           </example>
         </instructions>
-
-        <input>{question}</input>
-        <output>
+        <history>
+          {history}
+        </history>
+        <question>
+          {question}
+        </question>
     """
     """用户提示词"""
 
@@ -58,14 +82,39 @@ class QuestionRewrite(CorePattern):
         """问题补全与重写"""
         history = kwargs.get("history", [])
         question = kwargs["question"]
-
+        llm = kwargs.get("llm", None)
+        if not llm:
+            llm = ReasoningLLM()
+        leave_tokens = llm._config.max_tokens
+        leave_tokens -= TokenCalculator().calculate_token_length(
+            messages=[
+                {"role": "system", "content": self.system_prompt},
+                {"role": "user", "content": self.user_prompt.format(history="", question=question)}
+            ]
+        )
+        if leave_tokens <= 0:
+            logger.error("[QuestionRewrite] 大模型上下文窗口不足，无法进行问题补全与重写")
+            return question
+        index = len(history)-1
+        qa = ""
+        while index >= 1 and leave_tokens > 0:
+            q = history[index-1].get("content", "")
+            a = history[index].get("content", "")
+            sub_qa = f"<qa id>\n<question>\n{q}\n</question>\n<answer>\n{a}\n</answer>\n</qa>"
+            leave_tokens -= TokenCalculator().calculate_token_length(
+                messages=[
+                    {"role": "user", "content": sub_qa}
+                ],
+                pure_text=True
+            )
+            if leave_tokens >= 0:
+                qa = sub_qa + qa
+            index -= 2
         messages = [
             {"role": "system", "content": self.system_prompt},
-            {"role": "user", "content": self.user_prompt.format(question=question)},
+            {"role": "user", "content": self.user_prompt.format(history=qa, question=question)},
         ]
-        messages = history+messages
         result = ""
-        llm = ReasoningLLM()
         async for chunk in llm.call(messages, streaming=False):
             result += chunk
         self.input_tokens = llm.input_tokens
