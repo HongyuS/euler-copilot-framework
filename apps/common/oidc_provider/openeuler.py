3a4c4b80fe2e4e662cae3a124d559c31de16ec87
z30057876 <zhouweitong@huawei.com>
2025-10-13 20:29:03 +0800

    去除手机号验证

--- apps/common/oidc_provider/openeuler.py
+++ apps/common/oidc_provider/openeuler.py
@@ -87,10 +87,6 @@ class OpenEulerOIDCProvider(OIDCProviderBase):
             logger.info("[OpenEuler] 获取OIDC用户成功: %s", resp.text)
             result = resp.json()
 
-        if not result["phone_number_verified"]:
-            err = "Could not validate credentials."
-            raise RuntimeError(err)
-
         return {
             "user_sub": result["sub"],
         }
